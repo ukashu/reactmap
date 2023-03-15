88a2d5613935911f146b5b0f403a199562946597
Lukasz Filipiuk <lukaszfilipiuk123@gmail.com>
2023-03-15 14:55:56 +0100

    form functionality done for now::TODO add tests

--- frontend/src/components/Page.jsx
+++ frontend/src/components/Page.jsx
@@ -1,29 +1,52 @@
 import './Form.css'
 import DataColumn from './DataColumn'
+import { startTransition, useState } from 'react'
 
 function Page() {
   const calcData = JSON.parse(localStorage.getItem('calculated-flight'))
 
   console.log(calcData)
 
+  const [inputs, setInputs] = useState({
+    data: '',
+    'lotnisko-odlotu': '',
+    'lotnisko-docelowe': '',
+    'typ-statku': '',
+    'znaki-rejestracyjne': '',
+    'zgoda-odlot': '',
+    licznik: '',
+    start: '',
+    'zgoda-dolot': '',
+  })
+
+  function handleInputs(evt) {
+    evt.preventDefault()
+    setInputs((prevState)=>{
+      return {
+        ...prevState,
+        [evt.target.id]: evt.target.value
+      }
+    })
+  }
+
   return (
   <div className="page">
       <div className="subpage">
           <h2>OPERACYJNY PLAN LOTU</h2>
           <div id="data-box" className="data-box">
-            <div id="data" className="data"><p>DATA</p></div>
-            <div id="lotnisko-odlotu" className="lotnisko-odlotu"><p>LOTNISKO ODLOTU</p></div>
-            <div id="lotnisko-docelowe" className="lotnisko-docelowe"><p>LOTNISKO DOCELOWE</p></div>
-            <div id="typ-statku" className="typ-statku"><p>TYP STATKU</p></div>
-            <div id="znaki-rejestracyjne" className="znaki-rejestracyjne"><p>ZNAKI REJESTR.</p></div>
+            <div id="data" className="data"><p>DATA</p><textarea id="data"></textarea></div>
+            <div id="lotnisko-odlotu" className="lotnisko-odlotu"><p>LOTNISKO ODLOTU</p><textarea id="lotnisko-odlotu"></textarea></div>
+            <div id="lotnisko-docelowe" className="lotnisko-docelowe"><p>LOTNISKO DOCELOWE</p><textarea id="lotnisko-docelowe"></textarea></div>
+            <div id="typ-statku" className="typ-statku"><p>TYP STATKU</p><textarea id="typ-statku"></textarea></div>
+            <div id="znaki-rejestracyjne" className="znaki-rejestracyjne"><p>ZNAKI REJESTR.</p><textarea id="znaki-rejestracyjne"></textarea></div>
           </div>
           <div id="zgoda-1-box" className="zgoda-box">
-            <div id="zgoda-odlot"><p>ZGODA (ODLOT)</p></div>
+            <div id="zgoda-odlot" className="zgoda-odlot"><p>ZGODA (ODLOT)</p><textarea id="zgoda-odlot"></textarea></div>
             <div className="zgoda-container">
-              <div id="kierunek-wiatru" className="kierunek-wiatru"><p>KIERUNEK WIATRU</p></div>
-              <div id="tas" className="tas"><p>TAS</p></div>
-              <div id="licznik" className="licznik"><p>LICZNIK</p></div>
-              <div id="start" className="start"><p>START</p></div>
+              <div id="kierunek-wiatru" className="kierunek-wiatru"><p>KIERUNEK WIATRU</p><p style={{fontSize: "14px"}}>{calcData.constants.WTA}</p></div>
+              <div id="tas" className="tas"><p>TAS</p><p style={{fontSize: "14px"}}>{calcData.constants.TAS}</p></div>
+              <div id="licznik" className="licznik"><p>LICZNIK</p><textarea id="licznik" style={{height: "19px"}}></textarea></div>
+              <div id="start" className="start"><p>START</p><textarea id="start" style={{height: "19px"}}></textarea></div>
             </div>
           </div>
           <div id="trasa-box" className="trasa-box">
@@ -84,12 +107,12 @@ function Page() {
             <div></div>
           </div>
           <div id="zgoda-2-box" className="zgoda-box">
-            <div id="zgoda-odlot" className="zgoda-odlot"><p>ZGODA (DOLOT)</p></div>
-              <div className="zgoda-container">
-                <div id="godzina-ladowania" className="godzina-ladowania"><p>GODZ. LĄDOWANIA</p></div>
-                <div id="stan-licznika" className="stan-licznika"><p>STAN LICZNIKA</p></div>
-                <p>LOGO</p>
-              </div>
+            <div id="zgoda-odlot" className="zgoda-odlot"><p>ZGODA (DOLOT)</p><textarea id="zgoda-dolot"></textarea></div>
+            <div className="zgoda-container">
+              <div id="godzina-ladowania" className="godzina-ladowania"><p>GODZ. LĄDOWANIA</p><textarea id="godzina-ladowania" style={{height: "19px"}}></textarea></div>
+              <div id="stan-licznika" className="stan-licznika"><p>STAN LICZNIKA</p><textarea id="stan-licznika" style={{height: "19px"}}></textarea></div>
+              <p>LOGO</p>
+            </div>
           </div>
       </div>    
   </div>
