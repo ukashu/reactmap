82861219aed928901481dd9fca536295b88e3dd7
Lukasz Filipiuk <lukaszfilipiuk123@gmail.com>
2023-06-22 13:47:33 +0200

    for loop row generation instead of ugly temporary code

--- frontend/src/components/DataColumn.jsx
+++ frontend/src/components/DataColumn.jsx
@@ -5,19 +5,17 @@ function DataColumn(props) {
   //and display it in the form TODO
   console.log(props)
 
+  const generateRows = () => {
+    let rows = []
+    for (let i = 0; i < 10; i++) {
+      rows.push(<div>{(props.data.sections[i] && props.data.sections[i][props.display]) ? props.data.sections[i][props.display] : (props.data.sections[i] && props.data.constants[props.display] ? props.data.constants[props.display] : '')}</div>)
+    } return rows
+  }
+
   return (
     <div className="DataColumn">
       <div style={{display: "flex", justifyContent: "center", maxHeight: "35px"}}><p>{props.name.toUpperCase()}</p></div>
-      <div>{(props.data.sections[0] && props.data.sections[0][props.display]) ? props.data.sections[0][props.display] : (props.data.sections[0] && props.data.constants[props.display] ? props.data.constants[props.display] : '')}</div>
-      <div>{(props.data.sections[1] && props.data.sections[1][props.display]) ? props.data.sections[1][props.display] : (props.data.sections[1] && props.data.constants[props.display] ? props.data.constants[props.display] : '')}</div>
-      <div>{(props.data.sections[2] && props.data.sections[2][props.display]) ? props.data.sections[2][props.display] : (props.data.sections[2] && props.data.constants[props.display] ? props.data.constants[props.display] : '')}</div>
-      <div>{(props.data.sections[3] && props.data.sections[3][props.display]) ? props.data.sections[3][props.display] : (props.data.sections[3] && props.data.constants[props.display] ? props.data.constants[props.display] : '')}</div>
-      <div>{(props.data.sections[4] && props.data.sections[4][props.display]) ? props.data.sections[4][props.display] : (props.data.sections[4] && props.data.constants[props.display] ? props.data.constants[props.display] : '')}</div>
-      <div>{(props.data.sections[5] && props.data.sections[5][props.display]) ? props.data.sections[5][props.display] : (props.data.sections[5] && props.data.constants[props.display] ? props.data.constants[props.display] : '')}</div>
-      <div>{(props.data.sections[6] && props.data.sections[6][props.display]) ? props.data.sections[6][props.display] : (props.data.sections[6] && props.data.constants[props.display] ? props.data.constants[props.display] : '')}</div>
-      <div>{(props.data.sections[7] && props.data.sections[7][props.display]) ? props.data.sections[7][props.display] : (props.data.sections[7] && props.data.constants[props.display] ? props.data.constants[props.display] : '')}</div>
-      <div>{(props.data.sections[8] && props.data.sections[8][props.display]) ? props.data.sections[8][props.display] : (props.data.sections[8] && props.data.constants[props.display] ? props.data.constants[props.display] : '')}</div>
-      <div>{(props.data.sections[9] && props.data.sections[9][props.display]) ? props.data.sections[9][props.display] : (props.data.sections[9] && props.data.constants[props.display] ? props.data.constants[props.display] : '')}</div>
+      {generateRows()}
     </div>
   )
 }
